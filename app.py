f151b23bbad4919324fdb7c8fd6e005774b34b14
Vladislav <fl1ckje@mail.ru>
2023-10-26 17:54:02 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from web_parser import WebParser
+import os, sys, json
+from gevent.pywsgi import WSGIServer
+
+
+app = Flask(__name__)
+
+
+@app.route('/get-data', methods=['GET', 'POST'])
+def get_data():
+    if request.method == 'GET' or request.method == 'POST':
+        #получаем url из аргумента запроса
+        url = request.args.get('url')
+
+        #создаём экземпляр объекта парсера
+        parser = WebParser('firefox')
+
+        #получаем результат
+        result = parser.run(url)
+
+        #закрываем веб драйвер парсера
+        parser.driver.quit()
+
+        #сохраняем результат в json файл рядом со скриптом
+        working_dir = os.path.dirname(os.path.abspath(sys.argv[0])).replace('\\', '/')
+        with open(f"{working_dir}/{result['title']}.json", "w") as file: 
+            json.dump(result, file)
+
+        #возвращаем результат клиенту        
+        return result
+    else:
+        return 'Error. User GET or POST method for request'
+
+
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('127.0.0.1', 5000), app)
+    http_server.serve_forever()
+    # example = http://127.0.0.1:5000/get-data?url=your_link_to_product_page
---
+++ web_parser.py
@@ -0,0 +1,118 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions  import WebDriverException
+import time
+
+
+COOKIE_BTN_XPATH = '/html/body/div[3]/div/div[1]/div/div[2]/div/div[2]/div[2]/button'
+TITLE_XPATH = '//*[@id="pdp_product_title"]'
+SUBTITLE_XPATH = '/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[2]/div/div/div[1]/div/div[2]/div/h2'
+COLOR_XPATH = '/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[2]/div/div/span/div/div/ul/li[1]'
+STYLE_XPATH = '//*[@id="RightRail"]/div/span/div/div/ul/li[2]'
+DESCRIPTION_XPATH = '//*[@id="RightRail"]/div/span/div/div/p'
+PRICE_XPATH = '//*[@id="RightRail"]/div/div[1]/div/div[2]/div/div/div/div/div'
+SIZES_GRID_XPATH = '//*[@id="buyTools"]/div[1]/fieldset/div'
+IMG_CONTAINER_XPATH = '//*[@id="pdp-6-up"]'
+
+class WebParser():
+    def __init__(self, browser: str, url_load_timeout: int = 2):
+        try:
+            if browser == 'chrome':
+                options = webdriver.ChromeOptions()
+                options.accept_insecure_certs = True
+                driver = webdriver.Chrome(options)
+
+            elif browser == 'chromium edge':
+                driver = webdriver.ChromiumEdge()
+            elif browser == 'edge':
+                driver = webdriver.Edge()
+            elif browser == 'firefox':
+                driver = webdriver.Firefox()
+            elif browser == 'ie':
+                driver = webdriver.Ie()
+            elif browser == 'safari':
+                driver = webdriver.Safari()
+            else:
+                raise Exception('wrong web driver name')
+            
+            self.driver = driver
+            self.driver.maximize_window()
+
+            if url_load_timeout >= 2:
+                self.url_load_timeout = url_load_timeout
+            else:
+                self.url_load_timeout = 2
+
+        except Exception as e:
+            print(e)
+            return
+
+
+    def accept_cookies(self):
+        '''
+        принятие куки
+        '''
+        print('accepting cookies...')
+        try:
+            cookie_btn = self.driver.find_element(By.XPATH, COOKIE_BTN_XPATH)
+            cookie_btn.click()
+            print('cookie accept successful')
+
+        except WebDriverException:
+            print('cookie accept fail')
+
+
+    def run(self, url):
+        '''
+        запуск парсера
+        '''
+        product = {}
+
+        #загрузка страницы
+        self.driver.get(url)
+
+        #пауза для ожидания полной загрузки страницы
+        time.sleep(self.url_load_timeout)
+
+        #принятие куки
+        self.accept_cookies()
+
+        # получение наименования
+        product['title'] = self.driver.find_element(By.XPATH, TITLE_XPATH).get_attribute('innerHTML')
+
+        # получение типа
+        product['subtitle'] = self.driver.find_element(By.XPATH, SUBTITLE_XPATH).get_attribute('innerHTML')
+
+        # получение бренда
+        product['vendor'] = product['title'].split()[0]
+
+        # получение цвета
+        c = self.driver.find_element(By.XPATH, COLOR_XPATH).get_attribute('innerHTML')
+        product['color'] = c[c.find(':') + 2:]
+
+        # получение стиля
+        s = self.driver.find_element(By.XPATH, STYLE_XPATH).get_attribute('innerHTML')
+        product['style'] = s[s.find(':') + 2:]
+
+        # получение описания
+        product['description'] = self.driver.find_element(By.XPATH, DESCRIPTION_XPATH).get_attribute('innerHTML')
+
+        # получение цены
+        product['price'] = self.driver.find_element(By.XPATH, PRICE_XPATH).get_attribute('innerHTML').replace(',', '').replace('฿','')
+
+        # получение доступных размеров
+        sizes = []
+        for sizes_cell in self.driver.find_element(By.XPATH, SIZES_GRID_XPATH).find_elements(By.TAG_NAME, 'div'):
+            if sizes_cell.find_element(By.TAG_NAME, 'input').get_attribute('disabled') is None:
+                sizes.append(sizes_cell.find_element(By.TAG_NAME, 'label').get_attribute('innerHTML'))
+        product['sizes'] = sizes
+
+        # получение ссылок на фото
+        img_container = self.driver.find_element(By.XPATH, IMG_CONTAINER_XPATH)
+        imgs = img_container.find_elements(By.TAG_NAME, 'img')
+        photo_links = []
+        for img in imgs:
+            photo_links.append(img.get_attribute('src'))
+        product['photo_links'] = photo_links
+
+        return product
